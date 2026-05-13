0a7e94cba7989c8f70399bcc1d39b761a48616b5
ariel raymundo <ariraymundo98@gmail.com>
2026-05-13 14:15:16 -0700

    added classes, house and asset are done(?)

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,4 +1,76 @@
 package com.pluralsight;
 
-public class House {
+
+public class House extends Asset
+{
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public Double getValue()
+    {
+        if(this.condition == 4)
+        {
+            this.squareFoot = lotSize*lotSize;
+            return 180.00 + (this.squareFoot / .25);
+        }
+        if(this.condition == 3)
+        {
+            this.squareFoot = lotSize*lotSize;
+            return 130.00 + (this.squareFoot / .25);
+        }
+        if(this.condition == 2)
+        {
+            this.squareFoot = lotSize*lotSize;
+            return 90.00 + (this.squareFoot / .25);
+        }
+        if(this.condition == 1)
+        {
+            this.squareFoot = lotSize*lotSize;
+            return 80.00 + (this.squareFoot / .25);
+        }
+        return 0.00;
+    }
 }
